12f49e6136aef64deefc96cab93c145d9771bfbe
meric erkosun <meric.erkosun@gmail.com>
2024-09-17 19:04:43 +0300

    project 2 initialize and day 20 stuff

---
+++ Day20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Meriç Erkoşun on 17.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            HStack(alignment: .center){
+                Text("H")
+                Text("H")
+                Text("H")
+            }
+            HStack{
+                Text("H")
+                Text("H")
+                Text("H")
+            }
+            HStack{
+                Text("H")
+                Text("H")
+                Text("H")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day20/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Meriç Erkoşun on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
