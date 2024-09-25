be6f890ac00714fc636aa8c38c46359e85e70c90
meric erkosun <meric.erkosun@gmail.com>
2024-09-25 16:51:33 +0300

    day 23 project 3 preparation

---
+++ Day23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Meriç Erkoşun on 25.09.2024.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var useRedText = false
+    
+    var body: some View {
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+    }
+}
+
+#Preview {
+    ContentView()
+}
