48f0310866663d6c764688e2a1fab76e33f32d6d
meric erkosun <meric.erkosun@gmail.com>
2024-12-12 15:38:05 +0300

    iExpense app

---
+++ Day37/iExpense2/iExpense2/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  iExpense2
+//
+//  Created by Meriç Erkoşun on 12.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in // Buradaki \.id ExpenseItem property olandır. ExpenseItem structuna Identifiable protocol sağladığımız için burada id: kullanmaya gerek kalmadı.
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+//                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 61)
+//                    expenses.items.append(expense)
+                    showingAddExpense = true
+                }
+            }
+            .navigationTitle("iExpense")
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+    }
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day37/iExpense2/iExpense2/iExpense2App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense2App.swift
+//  iExpense2
+//
+//  Created by Meriç Erkoşun on 12.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpense2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
