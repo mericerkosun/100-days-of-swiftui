a071c1fc077356fd201487c33fcb03f8a59bb633
meric erkosun <meric.erkosun@gmail.com>
2024-12-13 15:13:29 +0300

    day 38 challenges on iExpense

--- Day37/iExpense2/iExpense2/AddView.swift
+++ Day37/iExpense2/iExpense2/AddView.swift
@@ -13,7 +13,9 @@ struct AddView: View {
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
-    var expenses: Expenses
+    var expensesOh: Expenses
+    var currency: Currency
+    
     
     let types = ["Business", "Personal"]
     
@@ -28,14 +30,14 @@ struct AddView: View {
                     }
                 }
                 
-                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                TextField("Amount", value: $amount, format: .currency(code: currency.currencyDictionary[currency.selectedCurrency] ?? "ERR"))
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
             .toolbar {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
-                    expenses.items.append(item)
+                    expensesOh.items.append(item)
                     dismiss()
                 }
             }
@@ -44,5 +46,5 @@ struct AddView: View {
 }
 
 #Preview {
-    AddView(expenses: Expenses())
+    AddView(expensesOh: Expenses(), currency: Currency())
 }
