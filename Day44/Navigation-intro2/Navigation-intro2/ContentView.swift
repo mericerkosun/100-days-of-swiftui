b4dc9a7d7261bd65c1cfdc1c79cc6511fe2b708d
meric erkosun <meric.erkosun@gmail.com>
2024-12-23 18:16:00 +0300

    navigationStack @Binding

---
+++ Day44/Navigation-intro2/Navigation-intro2/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Navigation-intro2
+//
+//  Created by Meriç Erkoşun on 23.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var path = [Int]()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
+                        .toolbar {
+                            Button("Home") {
+                                path.removeAll()
+                            }
+                        }
+                }
+        }
+    }
+}
+
+struct DetailView: View {
+    var number: Int
+    @Binding var path: [Int]
+
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day44/Navigation-intro2/Navigation-intro2/Navigation_intro2App.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_intro2App.swift
+//  Navigation-intro2
+//
+//  Created by Meriç Erkoşun on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_intro2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
